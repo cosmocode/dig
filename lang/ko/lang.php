3d64e0bda74b445a9f8d981cacd89e1c693658d1
Myeongjin <aranet100@gmail.com>
2013-12-09 16:37:00 +0900

    Add Korean translation

---
+++ lang/ko/lang.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Korean language file for dig plugin
+ *
+ * @author Myeongjin <aranet100@gmail.com>
+ */
+
+$lang['host'] = '호스트';
+$lang['ip']   = 'IP 주소';
+$lang['type'] = '유형';
+$lang['ttl']  = 'TTL';
+$lang['soa']  = 'SOA';
+$lang['mx']   = 'MX 서버';
+$lang['comment'] = '주석';
+
+
+
+//Setup VIM: ex: et ts=4 enc=utf-8 :
